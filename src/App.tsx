c6aca78da5fd6bf06ad3bee61290898454ed3f3b
Fellipe Oliveira <fellipeoliveira962@gmail.com>
2025-12-06 19:32:10 -0300

    Aula 31 e 32

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
 import { DefaultInput } from './components/DefaultInput'
 import { Cycles } from './components/Cycles'
+import { DefaultButton } from './components/DefaultButton'
+import { PlayCircleIcon } from 'lucide-react'
+import { Footer } from './components/Footer'
+
+
+
 
 import './styles/theme.css'
 import './styles/global.css'
+
+
 
 
 export function App() {
@@ -38,9 +46,13 @@ export function App() {
             </div>
 
             <div className="formRow">
-                <button>Enviar</button>
+                <DefaultButton icon={<PlayCircleIcon />} />
             </div>
         </form>
+    </Container>
+
+    <Container>
+        <Footer />
     </Container>
 
     </>
