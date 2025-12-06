0ca98459c23fd244e4d4af5ccb3b9fafe7673778
Fellipe Oliveira <fellipeoliveira962@gmail.com>
2025-12-06 19:03:50 -0300

    Aula 27,28,29,30

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { Container } from './components/Container'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
 import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
+import { Cycles } from './components/Cycles'
 
 import './styles/theme.css'
 import './styles/global.css'
+
 
 export function App() {
     return <>
@@ -23,8 +26,7 @@ export function App() {
     <Container>
         <form className='form' action=""> 
             <div className="formRow">
-                <label htmlFor="meuInput">task</label>
-                <input id='meuInput' type="text" />
+                <DefaultInput labelText='Qual o seu objetivo?' id='meuInput' type='text' placeholder='Digite algo'/>
             </div>
 
             <div className="formRow">
@@ -32,8 +34,7 @@ export function App() {
             </div>
 
             <div className="formRow">
-                <p>Ciclos</p>
-                <p>0 0 0 0 0 0</p>
+                <Cycles />
             </div>
 
             <div className="formRow">
