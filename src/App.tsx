c12b984ae3b48af2ea855a16879f1e34497c317b
Fellipe Oliveira <fellipeoliveira962@gmail.com>
2025-12-09 12:29:36 -0300

    Aula 33,34,35

--- src/App.tsx
+++ src/App.tsx
@@ -9,51 +9,49 @@ import { PlayCircleIcon } from 'lucide-react'
 import { Footer } from './components/Footer'
 
 
-
-
 import './styles/theme.css'
 import './styles/global.css'
 
 
-
-
 export function App() {
-    return <>
-    <Container>
-        <Logo/>
-    </Container>
+    
+    return (
+        <>
+            <Container>
+                <Logo/>
+            </Container>
 
-    <Container>
-        <Menu />
-    </Container>
+            <Container>
+                <Menu />
+            </Container>
 
-    <Container>
-        <CountDown />
-    </Container>
+            <Container>
+                <CountDown />
+            </Container>
 
-    <Container>
-        <form className='form' action=""> 
-            <div className="formRow">
-                <DefaultInput labelText='Qual o seu objetivo?' id='meuInput' type='text' placeholder='Digite algo'/>
-            </div>
+            <Container>
+                <form className='form' action=""> 
+                    <div className="formRow">
+                        <DefaultInput labelText='Qual o seu objetivo?' id='meuInput' type='text' placeholder='Digite algo'/>
+                    </div>
 
-            <div className="formRow">
-                <p>Lorem ipsum dolor sit amet.</p>
-            </div>
+                    <div className="formRow">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
 
-            <div className="formRow">
-                <Cycles />
-            </div>
+                    <div className="formRow">
+                        <Cycles />
+                    </div>
 
-            <div className="formRow">
-                <DefaultButton icon={<PlayCircleIcon />} />
-            </div>
-        </form>
-    </Container>
+                    <div className="formRow">
+                        <DefaultButton icon={<PlayCircleIcon />} />
+                    </div>
+                </form>
+            </Container>
 
-    <Container>
-        <Footer />
-    </Container>
-
-    </>
+            <Container>
+                <Footer />
+            </Container>
+        </>
+    );
 }
