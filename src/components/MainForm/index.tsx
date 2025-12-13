ed1920b1b91007f2427bf3b33859edbb634c3cc9
Fellipe Oliveira <fellipeoliveira962@gmail.com>
2025-12-13 09:41:58 -0300

    Aula 42, 43, 44, 45, 46

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,26 +1,31 @@
-import { PlayCircleIcon } from "lucide-react";
-import { DefaultButton } from "../DefaultButton";
-import { Cycles } from "../Cycles";
-import { DefaultInput } from "../DefaultInput";
+import { PlayCircleIcon } from 'lucide-react';
+import { DefaultButton } from '../DefaultButton';
+import { Cycles } from '../Cycles';
+import { DefaultInput } from '../DefaultInput';
 
 export function MainForm() {
   return (
-     <form className='form' action=""> 
-                        <div className="formRow">
-                            <DefaultInput labelText='Qual o seu objetivo?' id='meuInput' type='text' placeholder='Digite algo'/>
-                        </div>
-    
-                        <div className="formRow">
-                            <p>Lorem ipsum dolor sit amet.</p>
-                        </div>
-    
-                        <div className="formRow">
-                            <Cycles />
-                        </div>
-    
-                        <div className="formRow">
-                            <DefaultButton icon={<PlayCircleIcon />} />
-                        </div>
-                    </form>
-  )
+    <form className='form' action=''>
+      <div className='formRow'>
+        <DefaultInput
+          labelText='Qual o seu objetivo?'
+          id='meuInput'
+          type='text'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Próximo intervalo é de 25min</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
 }
