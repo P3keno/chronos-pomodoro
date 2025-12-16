006af5bbbd6e602abdfb3cbbf13555c4f8010156
Fellipe Oliveira <fellipeoliveira962@gmail.com>
2025-12-16 15:51:22 -0300

    Aula 52,53,54

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,22 +1,62 @@
-import { PlayCircleIcon } from 'lucide-react';
 import { DefaultButton } from '../DefaultButton';
-import { Cycles } from '../Cycles';
 import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import type { TaskModel } from '../../models/TaskModel';
+import { Cycles } from '../Cycles';
+import { PlayCircleIcon } from 'lucide-react';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     console.log('DEU CERTO');
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // Conferir
+        secondsRemaining, // Conferir
+        formattedSecondsRemaining: '00:00', // Conferir
+        task: [...prevState.task, newTask],
+      };
+    });
   }
 
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
-          labelText='Qual o seu objetivo?'
+          labelText='task'
           id='meuInput'
           type='text'
           placeholder='Digite algo'
+          ref={taskNameInput}
         />
       </div>
 
