973d491bcd4b863ccb04711fa5f0862077ace198
Fellipe Oliveira <fellipeoliveira962@gmail.com>
2025-12-24 15:03:18 -0300

    Aula 76

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,6 +9,7 @@ import { getNextCycle } from '../../utils/getNextCycle';
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 import { Tips } from '../Tips';
+import { showMessage } from '../../adapters/showMessage';
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -20,13 +21,14 @@ export function MainForm() {
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+    showMessage.dissmiss();
 
     if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
 
     if (!taskName) {
-      alert('Digite o nome da tarefa');
+      showMessage.error('Digite o nome da tarefa');
       return;
     }
 
@@ -42,9 +44,12 @@ export function MainForm() {
 
     dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
 
+    showMessage.success(`Tarefa "${taskName}" iniciada!`);
   }
 
   function handleInterruptTask() {
+    showMessage.dissmiss();
+    showMessage.error(`Tarefa interrompida!`);
     dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
