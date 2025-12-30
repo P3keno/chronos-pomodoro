ddf35d84069358b9762179e443bff0faba776c8b
Fellipe Oliveira <fellipeoliveira962@gmail.com>
2025-12-30 17:55:11 -0300

    Metade da aula 82

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,25 @@
+import { TrashIcon } from 'lucide-react';
+import { Container } from '../../components/Container';
+import { DefaultButton } from '../../components/DefaultButton';
+import { Heading } from '../../components/Heading';
+import { MainTemplate } from '../../templates/MainTemplate';
+import styles from './styles.module.css';
+
+export function History() {
+  return (
+    <MainTemplate>
+      <Container>
+        <Heading>
+          <span>History</span>
+          <span className={styles.buttonContainer}>
+            <DefaultButton icon={<TrashIcon />} />
+          </span>
+        </Heading>
+      </Container>
+
+      <Container>
+        <div className='responsiveTable'></div>
+      </Container>
+    </MainTemplate>
+  );
+}
--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -3,12 +3,13 @@ import { AboutPomodoro } from '../../pages/AboutPomodoro';
 import { NotFound } from '../../pages/NotFound';
 import { Home } from '../../pages/Home';
 import { useEffect } from 'react';
+import { History } from '../../pages/History';
 
 function ScrollToTop() {
   const { pathname } = useLocation(); // Componente que leva pro inicio da pagina, pro topo da pagina
 
   useEffect(() => {
-    window.scrollTo( {top: 0, behavior: 'smooth'});
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }, [pathname]);
 
   return null;
@@ -18,9 +19,9 @@ export function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} />{' '}
+        <Route path='/history/' element={<History />} />
         <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
-
         <Route path='*' element={<NotFound />} />
       </Routes>
       <ScrollToTop />
